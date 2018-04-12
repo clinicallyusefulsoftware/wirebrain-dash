730fe3b85db5946fb08c61e77c68819973bf95e5
deveson <deveson@talktalk.net>
2018-04-12 21:06:08 +0100

    splashscreen and homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  menuData = [
+    { title: 'Our Menu', pic: 'assets/imgs/soup1.jpg', pushPage: 'MenuPage' },
+    { title: 'Account', pic: 'assets/imgs/coffee-people3.jpg', pushPage: 'AccountPage' },
+    { title: 'About Us', pic: 'assets/imgs/coffee6.jpg', pushPage: 'AboutPage' },
+    {title:'Locations', pic:'assets/imgs/cafe2.jpg', pushPage: 'LocationsPage'}
+  ];
+
 
   constructor(public navCtrl: NavController) {
 
